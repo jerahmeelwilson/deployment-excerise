17726c7621016261abf70de8becf53fca38a65c6
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-17 15:07:16 -0600

    Added path for css

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.use("/", express.static(path.join(__dirname, "styles.css")));
+app.get("/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../styles.css"));
+});
 
 const port = process.env.PORT || 4005;
 
