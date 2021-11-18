adca89766d90420ca25c97307b3d38af422d36ee
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-18 15:28:26 -0600

    Added rollbar

--- server/index.js
+++ server/index.js
@@ -3,15 +3,24 @@ const path = require("path");
 
 const app = express();
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "33bd0a606a2140c08709b3dd4a9cd35f",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.get("/styles", (req, res) => {
+app.get("/style", (req, res) => {
   res.sendFile(path.join(__dirname, "../styles.css"));
 });
 
 const port = process.env.PORT || 4005;
+
+app.use(rollbar.errorHandler());
 
 app.listen(port, () => {
   console.log(`App running on port number ${port}`);
